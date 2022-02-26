c028408cd3d91381f11c78c31c1e3e326b639e4a
dbirdflyshi <dander92@me.com>
2022-02-26 17:23:28 -0500

    added enumerables lesson and the first exercise

---
+++ Intro3.rb
@@ -0,0 +1,274 @@
+################################################################################
+#                                                                              #
+#                           Enumerables & Ranges                               #
+#                                                                              #
+################################################################################
+######################################
+#               Lesson               #
+######################################
+############################
+#   Array Manipulation     #
+#         Methods          #
+############################
+
+# Building the array for the lesson
+arr = ['🐶','🐱','🐸','🐷'] # The gang has arrived
+puts arr
+puts
+
+#Shovel : << : adds an element to the end of an array, evaluates to the array
+puts arr << '🐛' # Adding this lil stinky boy to the party
+puts
+
+# Push : array_name.push() : adds an element to the end of an array, evaluates to the array. 
+# This is better than shovel << because it supports multiple at the same time
+puts arr.push('🐼','🐰') # BFFs have made an entrance
+puts
+
+# Pop removes the last element from an array, evaluates to the element that was removed
+puts arr.pop # Poor bunny got kicked out
+puts
+
+# Unshift adds an element to the front of an array, evaluates to the array
+puts arr.unshift('🐰') # Bunny makes a triumphant return to the front of the line
+puts
+
+# Shift removes the first element of an array, evalulates to the element that was removed
+puts arr.shift # Poor bunny just can't catch a break, he's out for cutting in line
+puts
+puts arr 
+puts 
+
+############################
+#     Index & Include      #
+#          Methods         #
+############################
+# Building the array for the lesson
+wardrobe = ['🎩','👔','👟','👖']
+dresser = 'sock'
+
+# Index : array.index(item) : prints the index position of an item in the list you want
+puts wardrobe.index('👔') # 1 because it's the first item in the wardrobe
+
+# Include? : array.inclue?(item) : evaluates a boolean indicating if the item is found
+puts wardrobe.include?('👘') # False because we don't have a kimono in our wardrobe, yet..
+puts wardrobe.include?('🎩') # True because we have a top hat in our wardrobe
+puts dresser.include?('ock') # True because the 'ock' exists as a substring in the string dresser 'sock'
+puts dresser.index('soc') # 0, shows where in the index the first letter of the substring exists
+
+############################
+#      Reverse Method      #
+############################
+# Building the array for the lesson
+forecast = ['🌤','🌦','🌧','⛈'] # This is what the weather will be today, nice and then crappy
+puts forecast
+puts
+
+# Reverse : array/string.reverse : evaluates to a new reversed version of the array/string
+reverse_forecast = forecast.reverse # Now our day begins crappy and ends nice
+puts reverse_forecast
+puts
+
+# Reverse! : array/string.reverse! : reverses an array/string in place
+reverse_forecast.reverse! # We'll bring it back to crappy in the end 
+# with the '!' at the end of 'reverse' we dont need to re-declare the variable.
+puts forecast
+puts 
+
+# Reversing strings
+word = 'Supercalifragilisticexpialidocious'
+puts word.reverse
+
+# Palindrome checker using reverse
+def is_palindrome(word)
+    if word == word.reverse
+        puts 'Palindrome'
+    else
+        puts 'Not palindrome'
+    end
+end
+
+is_palindrome('apple')
+
+############################
+#       Range Slicing      #
+#          Methods         #
+############################
+## Building the array for the lesson
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+            'p','q','r','s','t','u','v','w','x','y','z']
+
+# .. : array[start..end] : grabs elements including the end
+print alphabet[1..10] # ["b", "c", "d", "e", "f", "g", "h", "i", "j", "k"]
+puts 
+
+# ... : array[start...end] : grabs elements excluding the end
+print alphabet[1...10] # ["b", "c", "d", "e", "f", "g", "h", "i", "j"]
+puts
+
+## Using Strings
+str = 'alphabet'
+print str[1..4] # lpha
+puts
+
+# Using negative index positioning
+print str[3..-1] # habet
+puts 
+
+############################
+#        Split & Join      #
+#          Methods         #
+############################
+# Building the string for the lesson
+str = 'the lazy red fox jumps over the brown logs quickly'
+
+# Split : string.split(arg) : allows you to separate the string into individual 
+#                             elements of an array using a specified delimiter argument
+words = str.split(' ') # This allows you to convert a string with multiple words
+print words             # into an array of each word as an element
+puts # '["the", "lazy", "red", "fox", "jumps", "over", "the", "brown", "logs", "quickly"]'
+puts words[2] # 'red' : we're getting the 2nd index of the new words list
+
+letters = str.split('') # Quotes without a space in the argument allows you to 
+print letters           # make each element a letter in the string
+puts 
+
+# Join : string.join(arg) : allows you to concatenate an array into a single 
+#                           element using a specified delimiter argument
+puts words.join(' ') # 'the lazy red fox jumps over the brown logs quickly'
+
+# What if we want to find and replace text or characters in a string? These two
+# methods allow us to do this with relative ease
+def find_and_replace(string,find,replace)
+    split = string.split(find)
+    rejoined = split.join(replace)
+    return rejoined
+end
+puts find_and_replace('hello darkness my old friend',' ','-')
+
+
+############################
+#         Enumerable       #
+#          Methods         #
+############################
+# Building the array for the lesson
+months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
+
+# Use this if you only have one line worth of code
+months.each { |month| puts month }
+
+# Use this if you have multiple lines worth of code to run
+months.each do |month|
+    puts month 
+    puts '---'
+end 
+
+# If we want to use the index, we use 'each_with_index'
+months.each_with_index do |month, idx|
+    puts month 
+    puts idx
+    puts '---'
+end 
+
+# We can do the same with strings but we use 'each_char'
+sentence = 'it is too hot here'
+sentence.each_char do |char|
+    puts char
+end 
+
+# If we want to use the index, we do the same but we use 'each_char.with_index'
+sentence = 'it is too hot here'
+sentence.each_char.with_index do |char, idx|
+    puts char
+    puts idx
+    puts '---'
+end 
+
+############################
+#     Range Enumerable     #
+#          Methods         #
+############################
+## Building the array for the lesson
+alphabet = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o',
+            'p','q','r','s','t','u','v','w','x','y','z']
+
+# We can use the keyword 'each' to loop through a range
+arr.each { |ele| puts ele }
+
+# But what if we want to specify the range in the loop? We use (min..max)/(min...max)
+(1..10).each{ |num| puts num }
+
+# Let's revisit the FizzBuzz challenge from the last set of arrays exercises
+# Recap : write a method that takes in a number max and returns an array containing
+# all numbers less than max that are divisible by 3 or by but not both.
+def fizzBuzz(max)
+    arr = []
+    (1...max).each do |num|
+        if (num % 3 == 0 || num % 5 == 0) && !(num % 3 == 0 && num % 5 == 0)
+            arr << num
+        end
+    end
+    return arr 
+end
+puts fizzBuzz(20)
+
+############################
+#     Range Enumerable 2   #
+#          Methods         #
+############################
+# Making the range enumerable easier to write
+# num.times {<code>}
+#     -or-
+# num.times do 
+#   <code>
+# end 
+
+# Single line
+4.times {puts 'hello'}
+
+# Multiple lines
+4.times do 
+    puts 'hi'
+end 
+
+######################################
+#             Exercises              #
+######################################
+############################
+#        to Initials       #
+############################
+# Write a method that takes in a persons name string and returns a string
+# representing their initials
+
+def to_initials(name)
+  # Constructing the array
+  initials = []
+  # We first want to split out the names
+  name_parts = name.split(' ')
+  # Once we get the first and last names separated, we want to iterate each element in the new array
+  name_parts.each_with_index do |name_parts, idx|
+    # with each element iterating, we want only the first letter of each element and store it into the array
+  	initials << name_parts[0][0]
+  end
+  # Now that we have this array that looks like this ['K','B']
+  # we need to make this into a full text string again, we use join for that
+  initials = initials.join('')
+  # Returning the final output so its now a global variable
+  return initials
+end
+
+puts to_initials("Kelvin Bridges")      # => "KB"
+puts to_initials("Michaela Yamamoto")   # => "MY"
+puts to_initials("Mary La Grange")      # => "MLG"
+
+# This is the instructor's version and it's much more elegant
+def to_initials(name)
+  parts = name.split(" ")
+  initials = ""
+  parts.each { |part| initials += part[0] }
+  return initials
+end
+
+############################
+#      First In Array      #
+############################
